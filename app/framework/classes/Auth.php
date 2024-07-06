7a1d9aa8cf213f24960588ed53bc6651333eb06c
savio <saviomenezesbrito@gmail.com>
2024-07-06 10:50:41 -0300

    Block not logged in users

---
+++ app/framework/classes/Auth.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\framework\classes;
+
+class Auth
+{
+    public static function check(string $type) {
+        switch ($type) {
+            case 'auth':
+                if(!isset($_SESSION['logged'])) {
+                    return redirect('/');
+                }
+            break;
+        }
+    }
+}
